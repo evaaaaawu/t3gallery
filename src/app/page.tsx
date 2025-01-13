e5013beb0eb03be6cd9e696d8c535d7df632c4ed
evaaaaawu <yiju.wu1@gmail.com>
2025-01-13 18:43:35 +0800

    feat: scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+const mockUrls = [
+  "https://ewpy7eni1f.ufs.sh/f/2Xwpxzi4mQ5K0KYKN1PJcG4DU86pS9xQEbzjeBoL3ryXA1t2",
+  "https://ewpy7eni1f.ufs.sh/f/2Xwpxzi4mQ5KDvPZZXEO4n7FURTqJQ9rCWjdEODlyZ0SohHs",
+  "https://ewpy7eni1f.ufs.sh/f/2Xwpxzi4mQ5KJP2ywMppd5vo3q0RzmaFEKuwCO6sblNPgVf9",
+  "https://ewpy7eni1f.ufs.sh/f/2Xwpxzi4mQ5KqDUfLnHmVpJFwUSrf3Om4YabICTW6coixQPN"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       <h1>Hello (T3 Gallery in progress)</h1>
     </main>
   );
